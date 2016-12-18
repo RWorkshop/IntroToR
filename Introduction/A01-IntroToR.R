9fd593be036f1f2b26d44e4fd06e6e5091631dcd
DragonflyStats <kobriendublin@gmail.com>
2016-12-18 19:48:28 +0000

    Update A01-IntroToR.R

--- Introduction/A01-IntroToR.R
+++ Introduction/A01-IntroToR.R
@@ -7,17 +7,6 @@
 # Part v Basic statistical operations
 #########################
  
-x<-14
-y<-8
-
-functions
-sqrt()      - square root
-abs()       - Absolute value 
-exp()	   - The exponential
-log(,b)     - logarithm to the base "b". The default setting is the natural log.
- 
-factorial()       - computes factorials
-choose()         - computes binomial coefficients
 
 
 ##############################
@@ -29,18 +18,3 @@ These include the simplest approach, using the the colon ":", e.g.
 [1] 1 2 3 4 5 6 7 8 9 10
 
 
-###############################
-> X = 5:10
-> Y = 8:12
->
-> X
-[1]  5  6  7  8  9 10
->
-> Y
-[1]  8  9 10 11 12
->
-> union(X,Y)
-[1]  5  6  7  8  9 10 11 12
->
-> intersect(X,Y)
-[1]  8  9 10
